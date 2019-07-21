61e5a5350e008eea67df136142a75e63d2ccb907
Yujunw <691285799@qq.com>
2019-07-21 21:59:35 +0800

    2019/5/23 20:46

---
+++ c++/addTwoNumbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Solution:
+public:
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2){
+		ListNode *pHead = new ListNode(0);
+		ListNode *p1 = l1, *p2 = l2, *p3 = pHead;
+		ListNode *pNext;
+		int sum = 0, carry = 0;
+		while(p1 != NULL || p2 != NULL){
+			
+		}
+		
+	}
+		
