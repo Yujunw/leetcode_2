ff8c25046be78ea742f3e5cf9469d09f803b8874
Yujunw <691285799@qq.com>
2019-07-26 11:03:09 +0800

    2019/5/23 20:46

--- 22_括号生成.py
+++ 22_括号生成.py
@@ -1,8 +1,6 @@
 '''
 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
-
 例如，给出 n = 3，生成结果为：
-
 [
   "((()))",
   "(()())",
@@ -10,13 +8,29 @@
   "()(())",
   "()()()"
 ]
-
+回溯法可以被认为是一个有过剪枝的DFS过程
+生成各种组合的题目就使用回溯算法，深度优先搜索
+控制右括号的个数必须小于左括号
 '''
-
 
 class Solution:
     def generateParenthesis(self, n):
-        res = ['(']
-        n1, n2 = n-1, n-1
+        res = []
+        def backtrace(combination, left, right):
+            if len(combination) == 2*n:
+                res.append(combination)
+            # 这就是剪枝条件，左括号个数必须小于总数，右括号个数必须小于左括号
+            if left < n:
+                backtrace(combination+'(', left+1, right)
+            if right < left:
+                backtrace(combination+')', left, right+1)
+
+        backtrace('',0,0)
+        return res
+
+S = Solution()
+n = 0
+print(S.generateParenthesis(n))
+
         
 
